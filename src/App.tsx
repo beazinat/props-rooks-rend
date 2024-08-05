36417680cecaa32b1f9381ae0604b3da99a6c1ee
Beazinat <beatrizgarodrigues@gmail.com>
2024-08-05 16:30:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
+  return (
+    //  <Home  title="Componente Home"
+    //         description="Este é um componente Home que recebe props."/>
+    <div>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
+  );
+}
+
+export default App;
